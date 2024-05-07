d0d0d6bd33e1fd2124f742f91b58f0051ab1ec22
develcooking <jan-peterfriebe@gmail.com>
2024-05-07 15:10:46 +0200

    Revert "initial commit"
    This reverts commit 86c03b4435979e63952d6dbce6e9c0187db1376b.

--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
 fn main() {
-    let config =
-    slint_build::CompilerConfiguration::new()
-    .with_style("fluent".into());
-slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
+    slint_build::compile("ui/appwindow.slint").unwrap();
 }
