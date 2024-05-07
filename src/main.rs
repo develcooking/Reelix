f61e5ce8323a1c51c584c7a9f616e9408e9cedd7
develcooking <jan-peterfriebe@gmail.com>
2024-05-07 16:24:25 +0200

    new branch cmd-create-type

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
-slint::include_modules!();
+use mysql::*;
+use mysql::prelude::*;
+use std::io;
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = MainWindow::new()?;
+fn main() {
+    let database_url = "mysql://theuser:12345@141.13.222.38:3306/thebanking1";
 
-    //count upwards if button is pressed
-   /* ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });*/
+    let pool = Pool::new(database_url)
+        .expect("Failed to create a connection pool");
 
-    //will implement link opening
-   /* Tabsandmore.on_open_url(|url| {
-        open::that(url.as_str()).ok();
-    });*/
-    ui.run()
+    let mut conn = pool.get_conn()
+        .expect("Failed to get a connection from the pool");
+
+    println!("Bitte geben Sie den Typen ein:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Fehler beim Einlesen der Eingabe");
+    let Typen = input.trim();
+
+    conn.exec_drop(
+        r"INSERT INTO Typtabelle (Typen) VALUES (?)",
+        (&Typen,)
+    ).expect("Fehler beim Einfügen der Daten");
+    
+    println!("Daten erfolgreich eingefügt!");
 }
