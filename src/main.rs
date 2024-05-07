3ad85920722c56a8b9f4de9ed8562d601ed93d1a
develcooking <jan-peterfriebe@gmail.com>
2024-05-07 15:10:46 +0200

    Revert "initial commit"
    This reverts commit 86c03b4435979e63952d6dbce6e9c0187db1376b.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,20 @@
-use slint::{SharedString, VecModel};
 slint::include_modules!();
-use std::rc::Rc;
-use mysql::*;
-use mysql::prelude::*;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref POOL: Pool = {
-        let database_url: &str = "mysql://theuser:12345@141.13.222.38:3306/thebanking1";
-        Pool::new(database_url)
-            .expect("Failed to create a connection pool")
-    };
-}
-
-fn fetch_data_from_database() -> Result<Vec<String>, slint::PlatformError> {
-    let mut conn = POOL.get_conn()
-        .expect("Failed to get a connection from the pool");
-        let types: Vec<String> = conn
-        .query_map(
-            "SELECT Typen FROM Typtabelle ORDER BY Typen ASC",
-            |row: Row| row.get("Typen").unwrap(),
-        )
-        .map_err(|err| slint::PlatformError::from(format!("MySQL error: {}", err)))?;
-
-    Ok(types)
-}
 
 fn main() -> Result<(), slint::PlatformError> {
-    let data_from_db = fetch_data_from_database()?;
-
-    let mut shared_strings = Vec::new();
-    for typ in data_from_db {
-        shared_strings.push(SharedString::from(typ));
-    }
-
     let ui = MainWindow::new()?;
-    let model_rc = Rc::new(VecModel::from(shared_strings)).into();
-    ui.set_the_model(model_rc);
 
-    ui.run()?;
-    Ok(())
+    //count upwards if button is pressed
+   /* ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });*/
+
+    //will implement link opening
+   /* Tabsandmore.on_open_url(|url| {
+        open::that(url.as_str()).ok();
+    });*/
+    ui.run()
 }
