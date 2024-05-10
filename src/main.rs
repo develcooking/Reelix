f3a6a7961ef86f9f5a9b329c4b308c737db0b457
ba4ju8 <jan-peter.friebe@uni-bamberg.de>
2024-05-10 12:30:49 +0200

    added manage cateories with bit improfements

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,107 @@
+use slint::{SharedString, VecModel};
 slint::include_modules!();
+use std::rc::Rc;
+use mysql::*;
+use mysql::prelude::*;
+use lazy_static::lazy_static;
 
+lazy_static! {
+    static ref POOL: Pool = {
+        let database_url: &str = "mysql://theuser:12345@141.13.222.38:3306/thebanking1";
+        Pool::new(database_url)
+            .expect("Failed to create a connection pool")
+    };
+}
+
+fn fetch_data_from_database() -> Result<Vec<String>, slint::PlatformError> {
+    let mut conn = POOL.get_conn()
+        .expect("Failed to get a connection from the pool");
+        let types: Vec<String> = conn
+        .query_map(
+            "SELECT Typen FROM Typtabelle ORDER BY Typen ASC",
+            |row: Row| row.get("Typen").unwrap(),
+        )
+        .map_err(|err| slint::PlatformError::from(format!("MySQL error: {}", err)))?;
+
+    Ok(types)
+}
+fn remove_data_from_database(valueofcombobox: &str)  {
+    let mut conn = POOL.get_conn()
+        .expect("Failed to get a connection from the pool");
+    let typen = valueofcombobox.trim();
+
+    conn.exec_drop(
+        r"DELETE FROM Typtabelle WHERE Typen = ?",
+        (&typen,)
+    ).expect("Fehler beim Löschen der Daten");
+
+}
+
+fn update_database_display(ui: &MainWindow) -> Result<(), slint::PlatformError> {
+    let data_from_db = fetch_data_from_database()?;
+
+    let mut shared_strings = Vec::new();
+    for typ in data_from_db {
+        shared_strings.push(SharedString::from(typ));
+    }
+
+    let model_rc = Rc::new(VecModel::from(shared_strings)).into();
+    ui.set_the_model(model_rc);
+
+    Ok(())
+}
+
+fn createdata(input_text: &str) {
+    let trimmed_text = input_text.trim();
+    let filtered_text = trimmed_text.chars()
+        .collect::<String>();
+    
+    // Get a connection from the connection pool
+    let mut conn = POOL.get_conn()
+        .expect("Failed to get a connection from the pool");
+
+    println!("Original String: '{}'", input_text);
+    println!("Filtered String: '{}'", filtered_text);
+
+    // Insert the filtered text into the database if it's not empty
+    if !filtered_text.is_empty() {
+        conn.exec_drop(
+            r"INSERT INTO Typtabelle (Typen) VALUES (?)",
+            (&filtered_text,)
+        ).expect("Error inserting data");
+        
+        println!("Data inserted successfully!");
+    } else {
+        println!("Empty or whitespace-laden type string not inserted into the database.");
+    }
+}
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = MainWindow::new()?;
+    let ui_handle = ui.as_weak();
+    update_database_display(&ui);
 
-    //count upwards if button is pressed
-   /* ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });*/
+    let ui_handle_copy = ui_handle.clone();
 
-    //will implement link opening
-   /* Tabsandmore.on_open_url(|url| {
+    // Define a closure to create and insert data into the database based on user input
+    ui.on_cabavalueofcombobox(move |valueofcombobox| {
+       println!("removed {} from database", valueofcombobox);
+       remove_data_from_database(&valueofcombobox);
+       update_database_display(&ui_handle_copy.unwrap());
+
+    });
+
+    ui.on_createtype(move |newtypeinput| {
+        let extract_string: String = newtypeinput.trim().parse().unwrap();
+        createdata(&extract_string);
+        
+        update_database_display(&ui_handle.unwrap());
+    });
+    
+    ui.on_open_url(|url| {
         open::that(url.as_str()).ok();
-    });*/
-    ui.run()
+    });
+
+    ui.run()?;
+    Ok(())
 }
